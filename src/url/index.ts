d07598a1b46cdae2925e40e4b5311c2a25e4deba
JinMing6 <response965@163.com>
2024-05-25 18:20:10 +0800

    feat: 添加getFilenameFromDisposition

--- src/url/index.ts
+++ src/url/index.ts
@@ -12,3 +12,36 @@ export function getFilenameFromUrl(url: string): string {
   const arr = url.split('/');
   return arr[arr.length - 1];
 }
+
+/**
+ * 从content-disposition中获取文件ming
+ */
+export function getFilenameFromDisposition(
+  contentDisposition: string,
+): string | null {
+  if (!isString(contentDisposition)) return null;
+
+  // 尝试解析 `filename*`（使用RFC 5987编码）
+  const filenameStarMatch = contentDisposition.match(/filename\*=[^;]+/);
+  if (filenameStarMatch != null) {
+    const filenameStar = filenameStarMatch[0];
+    const parts = filenameStar.split("'");
+    if (parts.length === 3) {
+      return decodeURIComponent(parts[2]);
+    }
+  }
+
+  // 尝试解析 `filename`
+  const filenameMatch = contentDisposition.match(/filename="([^"]+)"/);
+  if (filenameMatch != null) {
+    return filenameMatch[1];
+  }
+
+  // 尝试解析未加引号的 `filename`
+  const filenameMatchUnquoted = contentDisposition.match(/filename=([^;]+)/);
+  if (filenameMatchUnquoted != null) {
+    return filenameMatchUnquoted[1];
+  }
+
+  return null;
+}
--- test/url.test.ts
+++ test/url.test.ts
@@ -1,8 +1,45 @@
-import { getFilenameFromUrl } from '../src/main';
+import { getFilenameFromDisposition, getFilenameFromUrl } from '../src/main';
 
 describe('url', () => {
   test('从url中获取文件名', () => {
     const url = 'https://www.baidu.com/abc.jpg';
     expect(getFilenameFromUrl(url)).toBe('abc.jpg');
+  });
+
+  it('应该去除Content-Disposition返回文件名的引号', () => {
+    const contentDisposition = 'attachment; filename="example.txt"';
+    const filename = getFilenameFromDisposition(contentDisposition);
+    expect(filename).toBe('example.txt');
+  });
+
+  it('应该从Content-Disposition返回未加引号的文件名', () => {
+    const contentDisposition = 'attachment; filename=example.txt';
+    const filename = getFilenameFromDisposition(contentDisposition);
+    expect(filename).toBe('example.txt');
+  });
+
+  it('应该从Content-Disposition返回带有编码filename*的文件名', () => {
+    const contentDisposition =
+      "attachment; filename*=UTF-8''%e4%b8%ad%e6%96%87.txt";
+    const filename = getFilenameFromDisposition(contentDisposition);
+    expect(filename).toBe('中文.txt');
+  });
+
+  it('如果Content-Disposition不包含filename，则返回null', () => {
+    const contentDisposition = 'attachment';
+    const filename = getFilenameFromDisposition(contentDisposition);
+    expect(filename).toBeNull();
+  });
+
+  it('如果Content-Disposition为空，应该返回null', () => {
+    const contentDisposition = '';
+    const filename = getFilenameFromDisposition(contentDisposition);
+    expect(filename).toBeNull();
+  });
+
+  it('如果Content-Disposition没有定义，应该返回null吗', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const filename = getFilenameFromDisposition(null as any);
+    expect(filename).toBeNull();
   });
 });
